66abebc15de454ad1c2fc7f162aa4e0841d0c55c
Harshad-desktop <harshadchavan141@gmail.com>
2020-06-26 21:59:49 +0530

    Completed the basics

--- index_basics.js
+++ index_basics.js
@@ -97,6 +97,8 @@ reusablewithargs(10,11); //fucntion invoking
 var myGlobal = 5;
 function myfunc1(){
     var mylocal = 10; //var is necessary to specify it as local scope/ fucntioan scope
+    console.log(mylocal) // only avaible in this fucntion local scope
+
     //mylocal = 10; //var if not written makes it global by default can be accessed anywhere in the program
 }
 function myfunc2(){
@@ -107,3 +109,91 @@ function myfunc2(){
 }
 myfunc1();
 myfunc2();
+
+
+
+//local variable of a fucntion has precedence over the global vaibale of the same name in a function
+// outside the fucntion if the variable is used it will be the global variable
+
+var myOuterwear = "T-Shirt";
+function mywear(){
+    var myOuterwear = "jeans";
+    return myOuterwear; //Returning a value from the fucntion
+}
+console.log(mywear()) //here it will be local
+console.log(myOuterwear) //here it will be global
+
+
+
+//when the function does not have a return statment it return undefined
+var value = 5;
+function addFive() {
+    value += 5
+}
+console.log(addFive()) //prints undefined here 
+console.log(value) //value of global variable changes
+
+
+//STAND IN LINE (a basic queue like structure)
+
+function nextInLine(arr,item){
+    arr.push(item)
+    return arr.shift()
+
+}
+var testarr = [1,2,3,4,5]
+console.log("before:" + JSON.stringify(testarr))
+console.log("item removed:" + nextInLine(testarr,6))
+console.log("before:" + JSON.stringify(testarr))
+
+
+//boolean values
+var flag = true//false
+
+function checkIftrue(flag){
+    if(flag){ return "it is true";}
+    else { return "it is false";}
+
+}
+console.log(checkIftrue(flag))
+
+//operators
+var val = 10
+
+function checkIfequal(val){
+    if(val == 10){ return "it is equal";}
+    else { return "not equal";}
+
+    //else if () statement order i very important
+
+}
+console.log(checkIfequal(val))
+
+//Note : ' == ' operator tries to convert the operands to the same datatype while comparing
+//       ' === ' operator also called strict comparison does not perform type conversion 
+console.log( 3 == '3')
+console.log( 3 === '3')
+
+console.log( 3 != '10') //inequality opreator
+console.log( 3 !== '10') //strict inequality
+
+//other operators > , < , >= , <= , or || , and &&
+
+
+//Switch statement
+var val = 2;
+
+function swtichcase(val) {
+    switch (val) { //compares with strict comparison ie without type conversion
+        case 1: var answer = "alpha"; break;
+        //case 1: var answer = "beta"; break; //cannot have cases with same name
+        case 2: var answer = "beta";  // if break not present it will propogate downwards
+        case 3: var answer = "gamma"; break;
+        default:  var answer = "stuff"; break;   
+    }
+    return answer
+}
+
+console.log(swtichcase(1))
+console.log(swtichcase(2))
+console.log(swtichcase(4))
